39b04b70912fdcbe2999e87647bc3ddf8028ab69
Yuxi Xue <yuxi200253@gmail.com>
2023-11-24 02:14:20 +0000

    更改vite配置文件

--- src/components/Configuration/config_url.js
+++ src/components/Configuration/config_url.js
@@ -1,3 +1,3 @@
 // All Endpoints for the application
-export const articlesURL =`http://51.144.181.79:5601/articles`;
-export const testURL =`http://localhost:3000/api.article`;
+export const articlesURL =`http://localhost:3000/api.article`;
+export const testURL =`http://51.144.181.79:5601/articles`;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port:3000,
+    proxy: {
+      '/articles': { 
+        target: 'http://40.68.89.87:5601', 
+        changeOrigin: true, 
+        ws: true,  
+     
+      }
+    },
+  },
+
 })
